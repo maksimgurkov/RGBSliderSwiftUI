9898ecd77d444ae10a8e39f0e225523d8be4729a
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-03 12:56:16 +0300

    Initial Commit

---
+++ RGBSliderSwiftUI/RGBSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSliderSwiftUIApp.swift
+//  RGBSliderSwiftUI
+//
+//  Created by Максим Гурков on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
